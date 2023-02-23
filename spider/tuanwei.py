84661e6c36d0a9690b9dab7dc41c0f8da6678a03
大磊 李 <191250067@smail.nju.edu.cn>
2023-02-23 19:58:38 +0800

    123

--- spider/tuanwei.py
+++ spider/tuanwei.py
@@ -1,4 +1,8 @@
 
-
 def test(key):
-    print(key)
+    with open("data.txt", 'r+') as f:
+        data = f.readlines()
+        for i in data:
+            if i.startswith(key):
+                print(i.split(':')[1])
+                break
